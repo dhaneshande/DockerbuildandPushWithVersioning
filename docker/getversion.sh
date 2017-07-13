978e423d496d6e8a163e458c95f808040600cf9f
dhaneshande <dande@miraclesoft.com>
2017-07-13 10:34:18 -0400

    Add files via upload

---
+++ docker/getversion.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+set -o errexit
+set -o errtrace
+
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]')
+  
+echo $ PACKAGE_VERSION
